735fc4a1c69b81283087229621195df7b6e12363
KS-7250 <45392149+KS-7250@users.noreply.github.com>
2023-09-26 17:14:16 -0400

    On branch test  Changes to be committed: 	new file:   .vscode/launch.json     new file:   .vscode/settings.json 	new file:   Resources/sshbanners.json 	modified:   honey.js 	new file:   improvedhoney.js 	new file:   test.js
    Added improved honeyfile along with new features that will be fleshed out in test branch.
    Working on adding ssh banner spoofing based on the server the script is ran on and a false
    login prompt that leads the attacker on and gets as much data on them as possible before terminating
    the connection and logging results.

--- honey.js
+++ honey.js
@@ -1,29 +1,52 @@
 const net = require('net');
 const fs = require('fs');
 
-const PORT = 22;
+const PORT = 22; // Change this to the desired port number
 const LOG_FILE = 'honeylog.txt';
-const CUSTOM_BANNER = 'SSH-2.0-OpenSSH_8.2p1 Ubuntu-4ubuntu0.5';
+const CUSTOM_BANNER = '';
 
 const server = net.createServer((socket) => {
+
   const remoteAddress = socket.remoteAddress;
   const remotePort = socket.remotePort;
 
-  console.log('Connection from: ${remoteAddress}:${remotePort}');
+  console.log(`Connection from: ${remoteAddress}:${remotePort}`);
 
-  const logEntry = 'Connection from: ${remoteAddress}:${remotePort}\n';
-  
+  // Log the connection details to honeylog.txt
+  const logEntry = `Connection from: ${remoteAddress}:${remotePort}\n`;
   fs.appendFile(LOG_FILE, logEntry, (err) => {
     if (err) {
-      console.error('Error writing to ${LOG_FILE}: ${err}');
+      console.error(`Error writing to ${LOG_FILE}: ${err}`);
     }
   });
 
-  socket.write(`${CUSTOM_BANNER}\r\n`);
+  // Read the client's SSH version message
+  let clientVersion = '';
+  socket.on('data', (data) => {
+    clientVersion += data.toString();
+    // Check if the client's SSH version message is complete
+    if (clientVersion.includes('\r\n')) {
+      // Log the SSH version (and potentially other details)
+      logSshVersion(remoteAddress, remotePort, clientVersion);
 
-  socket.end();
+      // Send a custom banner
+      socket.write('SSH-2.0-Server\r\n'); // Replace with your custom banner
+
+      // Close the connection
+      socket.end();
+    }
+  });
 });
 
+function logSshVersion(ip, port, version) {
+  const logEntry = `SSH version from ${ip}:${port}: ${version}`;
+  fs.appendFile(LOG_FILE, logEntry, (err) => {
+    if (err) {
+      console.error(`Error writing SSH version to ${LOG_FILE}: ${err}`);
+    }
+  });
+}
+
 server.listen(PORT, () => {
-  console.log('Listening on port ${PORT}...');
+  console.log(`Honeypot listening on port ${PORT}...`);
 });
